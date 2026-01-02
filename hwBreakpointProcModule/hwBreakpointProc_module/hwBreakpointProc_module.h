dcc4cd5eb6824dba777f027499234d44c8ddbe94
Renwuji666 <131771310+Renwuji666@users.noreply.github.com>
2026-01-02 22:58:56 +0800

    Add FAR to trace items

--- hwBreakpointProcModule/hwBreakpointProc_module/hwBreakpointProc_module.h
+++ hwBreakpointProcModule/hwBreakpointProc_module/hwBreakpointProc_module.h
@@ -107,12 +107,14 @@ struct TRACE_ITEM_PC {
 	uint64_t hit_time;
 	uint64_t pc;
 	uint64_t pstate;
+	uint64_t far;
 	uint64_t esr;
 };
 struct TRACE_ITEM_FULL {
 	uint64_t task_id;
 	uint64_t hit_time;
 	struct my_user_pt_regs regs_info;
+	uint64_t far;
 	uint64_t esr;
 };
 #pragma pack()
